15cb0158bb9a6e794a44a70bdc421f7d4f3a15f3
Eric Garside <eric@knewton.com>
2014-05-13 16:51:25 -0400

    Updating

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+ford build --skip
+cp lib/main/application/application.css output/application.min.css
+cp -r lib/main/application/images output/images
+cp -r movies output/movies
+mv output/index.html output/infomaze.html
+cp launcher.html output/index.html
+rm -rf ~/Desktop/frackingmaze.info
+mv output ~/Desktop/frackingmaze.info
+
