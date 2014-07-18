9b91c88109ae09493a3840bdf128a9ce4e5445cd
Eric Garside <eric@knewton.com>
2014-07-18 15:43:00 -0400

    Updating

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 ford build --skip
 cp lib/main/application/application.css output/application.min.css
-cp -r lib/main/application/images output/images
-cp -r movies output/movies
+#cat images.css >> output/application.min.css
+#cp -r lib/main/application/images output/images
+#cp -r movies output/movies
 mv output/index.html output/infomaze.html
+sed -i '' 's/application\.min/http:\/\/d1zd9zhjj7e0j.cloudfront.net\/fracking\/application.min/g' output/infomaze.html
+rm -rf output/application.min.css
+rm -rf output/application.min.js
 cp launcher.html output/index.html
-cp bigslide.jpg output/bigslide.jpg
+#cp bigslide.jpg output/bigslide.jpg
 rm -rf ~/Desktop/frackingmaze.info
 mv output ~/Desktop/frackingmaze.info
 
